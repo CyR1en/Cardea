acadd1fa2cf6c1dd1a2a4673b0dd93b008346e2b
cyr1en <cyrien3519@gmail.com>
2025-09-04 14:34:46 -0600

    Validate configuration against schema and handle invalid configurations

--- src/main/kotlin/com/cyr1en/cardea/Config.kt
+++ src/main/kotlin/com/cyr1en/cardea/Config.kt
@@ -4,10 +4,18 @@ import com.cyr1en.cardea.ColorPalette.*
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonObject
+import org.everit.json.schema.Schema
+import org.everit.json.schema.ValidationException
+import org.everit.json.schema.loader.SchemaLoader
+import org.json.JSONObject
+import org.json.JSONTokener
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.StandardOpenOption
+
+const val schemaUrl: String =
+    "https://raw.githubusercontent.com/CyR1en/Cardea/refs/heads/master/src/main/resources/cardea-config.schema.json"
 
 val gson: Gson = GsonBuilder()
     .setPrettyPrinting()
@@ -27,7 +35,11 @@ data class Config(
 data class DialogConfig(
     val timeout: Int = 60,
     val title: String = "<color:${MAUVE}>Cardea Login",
-    val body: List<String> = listOf("<color:${SUBTEXT1}>To prevent unauthorized access, please enter the server password below.", "", ""),
+    val body: List<String> = listOf(
+        "<color:${SUBTEXT1}>To prevent unauthorized access, please enter the server password below.",
+        "",
+        ""
+    ),
     val inputPrompt: String = "<color:${GREEN}>Enter password here",
     val buttons: ButtonConfig = ButtonConfig(),
     val loginResult: LoginResults = LoginResults()
@@ -74,15 +86,40 @@ fun serializeConfig(path: Path = defaultPath, config: Config, schemaUrl: String?
 fun deserializeConfig(path: Path = defaultPath): Config {
     return if (Files.exists(path)) {
         val json = Files.readString(path)
-        gson.fromJson(json, Config::class.java)
+        try {
+            validSchema(json)
+            gson.fromJson(json, Config::class.java)
+        } catch (e: ValidationException) {
+            logger.error("Invalid configuration!")
+            logger.error(e.errorMessage)
+            e.causingExceptions.map { it.message }.forEach {
+                logger.error(it)
+            }
+            logger.error("Using default config until fixed.")
+            Config()
+        }
     } else {
         val defaults = Config()
         serializeConfig(
             path,
             defaults,
-            "https://raw.githubusercontent.com/CyR1en/Cardea/refs/heads/master/src/main/resources/cardea-config.schema.json"
+            schemaUrl
         )
         defaults
+    }
+}
+
+/**
+ * Validates a JSON string against a predefined JSON schema.
+ *
+ * @param json the JSON string to be validated
+ * @throws [ValidationException] if [json] does not conform with the schema.
+ */
+fun validSchema(json: String) {
+    Cardea::class.java.getResourceAsStream("/cardea-config.schema.json").use { inputStream ->
+        val rawSchema = JSONObject(JSONTokener(inputStream))
+        val schema: Schema = SchemaLoader.load(rawSchema)
+        schema.validate(JSONObject(json))
     }
 }
 
